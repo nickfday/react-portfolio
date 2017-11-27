e2687ab45518585ab449085a6be93db4ee741dd5
Nick Finley-Day <nickfday@gmail.com>
2017-11-27 01:17:26 +0000

    Added home components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
+import { Projects } from "./Projects/Projects";
+import { Blog } from "./Blog";
 
 class App extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Welcome</h1>
-        <p>
-          React Web App is a simple web starter kit using react-create-app with
-          bootstrap 4 beta and a number of react components to give a
-          comfortable starting point.
-        </p>
-        <p>
-          This aims to save developers time when they wish to quickly spin-up a
-          web app and get to work rather than have to carry out the repetitive
-          chore of setting up. This can obviously be forked and customised to
-          the developers requirements.
-        </p>
-        <p>
-          It can be used as an example of how to consume components and give
-          developers new to react a stepping stone so they don't give up before
-          they have started.
-        </p>
-        <p>
-          At time of writing it is simply a web app with a Bootstrap v4 beta
-          theme and navigable using React Router v4. In the near future I plan
-          to add a basic blog component fed by a restful API.
-        </p>
-        <p>
-          <a
-            href="https://github.com/nickfday/react-bootstrap-and-go"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github Repo
-          </a>
-        </p>
+        <div className="intro">
+          <section className="container">
+            <h2>Welcome</h2>
+            <p>
+              We build professional web applications. From small-scale to large
+              enterprise solutions. Our projects are listed below.
+            </p>
+          </section>
+        </div>
+        <section>
+          <Projects slider="true" />
+        </section>
+
+        <section className="contact">
+          <Blog />
+        </section>
+
+        <section className="">
+          <div className="container">
+            <h2>Contact</h2>
+            <p>
+              If you’re interested in our services please email{" "}
+              <a href="mailto:nickfday@gmail.com">nickfday@gmail.com</a>.
+            </p>
+          </div>
+        </section>
       </div>
     );
   }
