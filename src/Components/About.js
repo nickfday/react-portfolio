3e96f2b05d6346813cbf109423a54e00346c50c0
Nick Finley-Day <nickfday@gmail.com>
2017-11-27 01:14:18 +0000

    Added about content

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,20 +3,33 @@ import { Follow } from "react-twitter-widgets";
 
 function About() {
   return (
-    <div className="about">
+    <div className="about container">
       <h1>About</h1>
       <p>
-        I am a full-time web developer based in London. I have a passion for
-        frontend development as well as backend Drupal dev.
+        Finley-Day Digital is passionate about everything digital. We specialise
+        in building web applications that work beautifully in any device. We
+        embrace open source technology. The majority of our sites are powered by
+        Drupal and WordPress.
       </p>
-      <p>Feel free to contact me on Twitter:</p>
-      <Follow
-        username="nickfday"
-        options={{
-          showCount: false,
-          size: "large"
-        }}
-      />
+      <p>
+        We take pride in our ability to work on both the creative and technical
+        sides of the digital industry. Finley-Day Digital has been operating
+        since 2009. During that time the industry has undergone substantial
+        changes. We embrace change and are quick to offer these services if we
+        feel they can add value for our client’s.
+      </p>
+
+      <p>
+        Design is very important to us. We love to see visions come to life. We
+        build our applications so that anything is possible.
+      </p>
+
+      <p>
+        Code is the glue that powers web applications. We pride ourselves on
+        adhering to high coding standards and rigorous testing. This means your
+        website or app will run as expected and is scalable so new features can
+        be seamlessly added.
+      </p>
     </div>
   );
 }
